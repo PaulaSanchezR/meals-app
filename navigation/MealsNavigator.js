d2c811bff4a4cc9f6bfb0bb4ff6e0d2edc5cfd66
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2020-04-21 21:46:59 -0400

    botton tap

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -3,11 +3,13 @@
 import { Platform } from 'react-native'
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator} from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import Colors from '../constants/Colors'
 
 import CategoriesScreen from '../screen/CategoriesScreen'
 import CategoryMealsScreen from '../screen/CategoryMealsScreen'
 import MealDetailScreen from '../screen/MealsDetailScreen'
+import FavoritesScreen from '../screen/FavoritesScreen'
 
 // createStackNavigator use two arguments 
 // firs argument are the screens object
@@ -34,7 +36,17 @@ const MealNavigator = createStackNavigator({
        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
          
     }
+});
+
+// the MealNavigator is nested here you can combine multiple navitator
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: { screen:MealNavigator },
+    Favorites: { screen:FavoritesScreen }
+},{
+    tabBarOptions:{
+      activeTintColor: Colors.accentColor
+    }
 })
 
 // appcontainer you wrap your root main navigator 
-export default createAppContainer(MealNavigator);
+export default createAppContainer(MealsFavTabNavigator);
